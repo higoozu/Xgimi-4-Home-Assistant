c8027122774af6a9d3afe6c86b5a933ed711e172
hugoozu <13860401+higoozu@users.noreply.github.com>
2025-02-27 10:28:45 +0800

    update some new actions

--- custom_components/xgimi_media_player/media_player.py
+++ custom_components/xgimi_media_player/media_player.py
@@ -1,6 +1,6 @@
 import logging
 from homeassistant.components.media_player import MediaPlayerEntity, DEVICE_CLASS_TV
-from homeassistant.const import STATE_OFF, STATE_ON
+from homeassistant.const import STATE_OFF, STATE_ON, STATE_PAUSED, STATE_PLAYING
 
 from .pyxgimi import XgimiApi
 
@@ -52,26 +52,33 @@ class XgimiMediaPlayer(MediaPlayerEntity):
             SUPPORT_TURN_OFF,
             SUPPORT_PLAY,
             SUPPORT_PAUSE,
+            SUPPORT_STOP,
             SUPPORT_VOLUME_STEP,
             SUPPORT_VOLUME_MUTE,
             SUPPORT_PREVIOUS_TRACK,
             SUPPORT_NEXT_TRACK,
+            SUPPORT_PLAY_MEDIA,
+            SUPPORT_SELECT_SOURCE,
         )
         return (
             SUPPORT_TURN_ON
             | SUPPORT_TURN_OFF
             | SUPPORT_PLAY
             | SUPPORT_PAUSE
+            | SUPPORT_STOP
             | SUPPORT_VOLUME_STEP
             | SUPPORT_VOLUME_MUTE
-            | SUPPORT_PREVIOUS_TRACK
-            | SUPPORT_NEXT_TRACK
+            | SUPPORT_PREVIOUS_TRACK  # left
+            | SUPPORT_NEXT_TRACK      # right
+            | SUPPORT_PLAY_MEDIA
+            | SUPPORT_SELECT_SOURCE   # home 或 menu
         )
 
     async def async_update(self):
         """Fetch new state data for this media player."""
         await self._api.async_fetch_data()
         self._state = STATE_ON if self._api.is_on else STATE_OFF
+        # 注意：play/pause 状态需要设备支持反馈，否则无法准确更新
 
     async def async_turn_on(self):
         """Turn the media player on."""
@@ -83,20 +90,14 @@ class XgimiMediaPlayer(MediaPlayerEntity):
         await self._api.async_send_command("poweroff")
         self._state = STATE_OFF
 
-    async def async_play_media(self, media_type, media_id, **kwargs):
-        """Play media."""
-        await self._api.async_send_command("play")
-        self._state = STATE_ON
-
-    async def async_media_play(self):
-        """Play the media player."""
-        await self._api.async_send_command("play")
-        self._state = STATE_ON
-
-    async def async_media_pause(self):
-        """Pause the media player."""
-        await self._api.async_send_command("pause")
-        self._state = STATE_ON
+    async def async_toggle(self):
+        """Toggle the power state."""
+        if self._state == STATE_OFF:
+            await self._api.async_send_command("poweron")
+            self._state = STATE_ON
+        else:
+            await self._api.async_send_command("poweroff")
+            self._state = STATE_OFF
 
     async def async_volume_up(self):
         """Increase the volume."""
@@ -110,10 +111,63 @@ class XgimiMediaPlayer(MediaPlayerEntity):
         """Mute the volume."""
         await self._api.async_send_command("volumemute")
 
+    async def async_media_play(self):
+        """Play the media player."""
+        await self._api.async_send_command("play")
+        self._state = STATE_PLAYING
+
+    async def async_media_pause(self):
+        """Pause the media player."""
+        await self._api.async_send_command("pause")
+        self._state = STATE_PAUSED
+
+    async def async_media_stop(self):
+        """Stop the media player (mapped to pause)."""
+        await self._api.async_send_command("pause")
+        self._state = STATE_PAUSED
+
+    async def async_media_play_pause(self):
+        """Toggle play/pause based on state."""
+        if self._state == STATE_PLAYING:
+            await self._api.async_send_command("pause")
+            self._state = STATE_PAUSED
+        else:
+            await self._api.async_send_command("play")
+            self._state = STATE_PLAYING
+
     async def async_media_previous_track(self):
-        """Send back command."""
+        """Send left command (mapped to previous track)."""
         await self._api.async_send_command("left")
 
     async def async_media_next_track(self):
-        """Send next command."""
+        """Send right command (mapped to next track)."""
         await self._api.async_send_command("right")
+
+    async def async_play_media(self, media_type, media_id, **kwargs):
+        """Play media (currently just triggers play)."""
+        await self._api.async_send_command("play")
+        self._state = STATE_PLAYING
+
+    async def async_select_source(self):
+        """Select input source (mapped to home)."""
+        await self._api.async_send_command("home")
+
+    async def async_send_command(self, command, **kwargs):
+        """Send a custom command to the device."""
+        valid_commands = [
+            "up", "down", "left", "right", "back", "home", "menu",
+            "play", "pause", "power", "volumedown", "volumeup",
+            "poweron", "poweroff", "volumemute"
+        ]
+        if command in valid_commands:
+            await self._api.async_send_command(command)
+            if command in ["play"]:
+                self._state = STATE_PLAYING
+            elif command in ["pause"]:
+                self._state = STATE_PAUSED
+            elif command == "poweron":
+                self._state = STATE_ON
+            elif command == "poweroff":
+                self._state = STATE_OFF
+        else:
+            _LOGGER.warning("Unsupported command: %s", command)
